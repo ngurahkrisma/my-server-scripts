ce4af844b879fd7d43f438b770a2350bf2d0fcef
ngurahkrisma <112155884+ngurahkrisma@users.noreply.github.com>
2026-03-14 11:44:57 +0800

    Update disk_report.sh

--- disk_report.sh
+++ disk_report.sh
@@ -1,2 +1,7 @@
-df -h / /dev/sdb1 --output=size,used,avail,pcent | grep -v "Size"
-uptime -p
+#!/bin/bash
+# Pindah ke folder script dan tarik update terbaru dari GitHub
+cd ~/scripts && git pull origin main > /dev/null 2>&1
+
+# Cek kapasitas System (/) dan Drive HDD (/mnt/Storage1)
+# Kita gunakan --output agar formatnya konsisten untuk dibaca n8n
+df -h / /mnt/Storage1 --output=size,used,avail,pcent | grep -v "Size"
